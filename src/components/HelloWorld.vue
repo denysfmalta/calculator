04e817c12ba84c3ce76b387c15545b32748078ae
denysfmalta <denysfmalta@hotmail.com>
2023-03-11 13:47:49 -0300

    started calculator project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Calculadora</h1>
+  <h3>Denys Ferreira Malta</h3>
+  <div class="calculator">
+    <div class="display">
+      {{ current || 0 }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<style scoped></style>
